6c086d2cedb81f0519712e60fccbd88babfab33b
did9984 <113024115+did9984@users.noreply.github.com>
2023-02-23 10:13:33 +0900

    Update MainExample.java

--- teamTest/src/main/java/teamTest/MainExample.java
+++ teamTest/src/main/java/teamTest/MainExample.java
@@ -3,5 +3,6 @@ package teamTest;
 public class MainExample {
   public static void main(String[] args) {
 	System.out.println("HELLO!123 TEAM PROJECT! - YEDAM!");
+	System.out.println("HELLO!123444 TEAM PROJECT! - YEDAM!");
 }
 }
